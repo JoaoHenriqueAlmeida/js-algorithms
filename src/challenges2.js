261dbda34d3b1e69e4ca51bbf6b6f195000559f8
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-10-22 22:03:32 -0300

    Req 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,25 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArr, name) {
+  return (techArr.length === 0) ? 'Vazio!' : techArr.sort().map((tech) => ({
+    tech,
+    name,
+  }));
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numberArray) {
+  // const elementOcurrences = numberArray.reduce(function (acc, curr) {
+  //   const verification = acc[curr] ? acc[curr] + 1 : acc[curr] = 1;
+  //   return verification;
+  // }, {});
+  // const getInvalidOcurrences = Object.values(elementOcurrences).filter((element) => element > 3);
+
+  // if (getInvalidOcurrences.length > 0 || numberArray.map((number) => (number > 9 || number < 0))) {
+  //   return 'não é possível gerar um número de telefone com esses valores';
+  // }
+  // if (numberArray.length !== 11) {
+  //   return 'Array com tamanho incorreto.';
+  // }
 }
 
 // Desafio 12
